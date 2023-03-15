8061d7cbc9da722e7005c44d2b24f2879ffbaa14
Govard9 <web-supsu@yandex.ru>
2023-03-15 14:40:29 +0700

    feat: создать навигационное меню в header.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../index.css';
+import Navigation from "./NavMenu";
+
+function App(props) {
+  return (
+    <>
+      <section className="header">
+        <Navigation />
+      </section>
+    </>
+  );
+}
+
+export default App;
