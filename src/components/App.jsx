fd512d0e674bfc9f04fef5e42fa0cd67380753ff
Govard9 <web-supsu@yandex.ru>
2023-03-27 14:51:18 +0700

    feat: коммит для переноса на ноут.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import '../index.css';
 import Navigation from "./NavMenu";
 import TextHeader from "./TextHeader";
+import ImageHeader from "./ImageHeader";
 
 function App(props) {
   return (
     <>
-      <section className="header">
+      <section className="nav">
         <Navigation />
+      </section>
+      <section className="header">
         <TextHeader/>
+        <ImageHeader/>
       </section>
     </>
   );
---
+++ src/components/ImageHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import photoOne from '../images/image-header-1.png';
+import photoTwo from '../images/image-header-2.png';
+import photoThree from '../images/image-header-3.png';
+
+function ImageHeader() {
+  return (
+    <div className="header-image">
+      <img className="header-image__photo-one" src={photoOne} alt=" Фото девушки в очках с синими волосами."/>
+      <img className="header-image__photo-two" src={photoTwo} alt=" Фото девушки в шляпе."/>
+      <img className="header-image__photo-three" src={photoThree} alt=" Фото девушки с совмещенными кулаками."/>
+    </div>
+  );
+}
+
+export default ImageHeader;
